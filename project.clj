7fe20291edf153c135d478460c8b972902197180
tbatchelli MBP <tbatchelli@acm.org>
2010-10-18 12:09:13 -0700

    Add log4j dependency in the project. (How did I miss that!?)

--- project.clj
+++ project.clj
@@ -2,5 +2,6 @@
   :description "Manage local VMs from the REPL"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [org.clojars.tbatchelli/vboxws_java16 "3.2.10"]]
+                 [org.clojars.tbatchelli/vboxws_java16 "3.2.10"]
+                 [log4j/log4j "1.2.14"]]
   :dev-dependencies [[swank-clojure/swank-clojure "1.2.1"]])

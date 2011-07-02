d5700ec32cee8a29512583cbd684a6bfdb4130ad
Antoni Batchelli <tbatchelli@acm.org>
2011-07-01 22:04:54 -0700

    Start 0.2.4-SNAPSHOT (one commit too late).

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject vmfest "0.2.3"
+(defproject vmfest "0.2.4-SNAPSHOT"
   :description "Manage local VMs from the REPL"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]

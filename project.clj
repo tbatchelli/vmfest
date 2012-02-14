374931388ae93ed46f5cd23c5e51e5423a2b1c06
Antoni Batchelli <tbatchelli@acm.org>
2012-02-14 13:38:31 -0800

    Fix the multi-dependencies set-up to provide a default dependency list

--- project.clj
+++ project.clj
@@ -1,13 +1,16 @@
 (defproject vmfest "0.2.4-SNAPSHOT"
   :description "Manage local VMs from the REPL"
-  :dependencies []
-  :multi-deps { "1.2" [[org.clojure/clojure "1.2.1"]]
-                "1.3" [[org.clojure/clojure "1.3.0"]]
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [slingshot "0.10.0"]
+                 [org.clojure/tools.logging "0.2.3"]
+                 [org.clojars.tbatchelli/vboxjws "4.1.8"]
+                 [fs "1.0.0"]]
+  :multi-deps {"1.3" [[org.clojure/clojure "1.3.0"]]
                 ;; "1.4" [[org.clojure/clojure "1.4.0-beta1"]]
                 :all [[slingshot "0.10.0"]
-                       [org.clojure/tools.logging "0.2.3"]
-                       [org.clojars.tbatchelli/vboxjws "4.1.8"]
-                       [fs "1.0.0"]]}
+                      [org.clojure/tools.logging "0.2.3"]
+                      [org.clojars.tbatchelli/vboxjws "4.1.8"]
+                      [fs "1.0.0"]]}
   :dev-dependencies [[robert/hooke "1.1.2"]
                      [log4j/log4j "1.2.14"]
                      [lein-clojars "0.6.0"]]

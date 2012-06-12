2d1e51d875920233df32d90692898719bfd7b3ae
Antoni Batchelli <tbatchelli@acm.org>
2012-06-12 12:07:57 +0200

    Add Eclipse Licensing information.

--- project.clj
+++ project.clj
@@ -5,12 +5,14 @@
                  [org.clojure/tools.logging "0.2.3"]
                  [org.clojars.tbatchelli/vboxjws "4.1.8"]
                  [fs "1.0.0"]]
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :multi-deps {"1.3" [[org.clojure/clojure "1.3.0"]]
-                ;; "1.4" [[org.clojure/clojure "1.4.0-beta1"]]
-                :all [[slingshot "0.10.0"]
-                      [org.clojure/tools.logging "0.2.3"]
-                      [org.clojars.tbatchelli/vboxjws "4.1.8"]
-                      [fs "1.0.0"]]}
+               ;; "1.4" [[org.clojure/clojure "1.4.0-beta1"]]
+               :all [[slingshot "0.10.0"]
+                     [org.clojure/tools.logging "0.2.3"]
+                     [org.clojars.tbatchelli/vboxjws "4.1.8"]
+                     [fs "1.0.0"]]}
   :dev-dependencies [[robert/hooke "1.1.2"]
                      [log4j/log4j "1.2.14"]
                      [lein-clojars "0.6.0"]]
